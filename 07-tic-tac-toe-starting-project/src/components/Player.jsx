dc4b439c4d847179ce067b704df2a15f19a14c79
bitibizi <hanbit199@gmail.com>
2024-08-29 12:27:41 +0200

    change the input on text box and save the changing

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}){
+export default function Player({initialName, symbol}){
+
+    const[playerName, setPlayerName]=useState(initialName);
     
     const[isEditing, setIsEditing]=useState(false)
     function handleEditClick(){
-        setIsEditing(true);
+        setIsEditing((editing)=>!editing);
     }
-    let playerName= <span className="player-name">{name}</span>
-    if(isEditing){
-        playerName=<input type="text" required/>
+
+    function handleChange(event){
+        setPlayerName(event.target.value)
     }
     
-    return( <li><span className="player">
-       {playerName}
-        <span className="player-symbol">{symbol}X</span></span>
-        <button onClick={handleEditClick}>Edit</button>
+    let editablePlayerName= <span className="player-name">{playerName}</span>
+    //let btnCaption="Edit"
+    if(isEditing){
+        editablePlayerName=<input type="text" required value={playerName} onChange={handleChange}/>
+        //let btnCaption="Save"
+    }
+    
+    return( 
+    <li>
+        <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+        </span>
+        
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+        
         </li>);
 }
